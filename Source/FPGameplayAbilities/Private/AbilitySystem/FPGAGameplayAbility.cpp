e39c751777c08c44d8feb4f4db5ce3e250605e12
fpwong <func.pwong@gmail.com>
2022-07-13 18:43:06 +1000

    Implement FPGAGameplayAbility implementing ActivateAbilityWithTargetData

---
+++ Source/FPGameplayAbilities/Private/AbilitySystem/FPGAGameplayAbility.cpp
@@ -0,0 +1,129 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AbilitySystem/FPGAGameplayAbility.h"
+
+#include "AbilitySystemComponent.h"
+
+UFPGAGameplayAbility::UFPGAGameplayAbility()
+{
+	auto ImplementedInBlueprint = [](const UFunction* Func) -> bool
+	{
+		return Func && ensure(Func->GetOuter())
+			&& Func->GetOuter()->IsA(UBlueprintGeneratedClass::StaticClass());
+	};
+
+	{
+		static FName FuncName = FName(TEXT("K2_ActivateAbilityWithTargetData"));
+		UFunction* ShouldRespondFunction = GetClass()->FindFunctionByName(FuncName);
+		bHasBlueprintActivateAbilityWithTargetData = ImplementedInBlueprint(ShouldRespondFunction);
+	}
+
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+}
+
+void UFPGAGameplayAbility::ActivateLocalPlayerAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	if (TriggerEventData)
+	{
+		NotifyTargetDataReady(TriggerEventData->TargetData, FGameplayTag());
+	}
+}
+
+void UFPGAGameplayAbility::ActivateServerAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	UAbilitySystemComponent* ASC = ActorInfo->AbilitySystemComponent.Get();
+	check(ASC);
+
+	// plug into AbilitySystemComponent to receive Target Data from client
+	NotifyTargetDataReadyDelegateHandle = ASC->AbilityTargetDataSetDelegate(Handle, ActivationInfo.GetActivationPredictionKey()).AddUObject(this, &ThisClass::NotifyTargetDataReady);
+}
+
+void UFPGAGameplayAbility::ActivateAbilityWithTargetData(const FGameplayAbilityTargetDataHandle& TargetDataHandle, FGameplayTag ApplicationTag)
+{
+	if (bHasBlueprintActivateAbilityWithTargetData)
+	{
+		K2_ActivateAbilityFromTargetData(TargetDataHandle, ApplicationTag);
+	}
+}
+
+void UFPGAGameplayAbility::NotifyTargetDataReady(const FGameplayAbilityTargetDataHandle& InData, FGameplayTag ApplicationTag)
+{
+	UAbilitySystemComponent* ASC = CurrentActorInfo->AbilitySystemComponent.Get();
+	check(ASC);
+
+	// [xist] is this (from Lyra) like an "if is handle valid?" check? seems so, keeping it as such.
+	if (!ASC->FindAbilitySpecFromHandle(CurrentSpecHandle))
+	{
+		CancelAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, false); // do not replicate
+		return;
+	}
+
+	// if commit fails, cancel ability
+	if (!CommitAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo))
+	{
+		CancelAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true); // replicate cancellation
+		return;
+	}
+
+	// true if we need to replicate this target data to the server
+	const bool bShouldNotifyServer = CurrentActorInfo->IsLocallyControlled() && !CurrentActorInfo->IsNetAuthority();
+
+	// Start a scoped prediction window
+	FScopedPredictionWindow ScopedPrediction(ASC);
+
+	// Lyra does this memcopy operation; const cast paranoia is real. We'll keep it.
+	// Take ownership of the target data to make sure no callbacks into game code invalidate it out from under us
+	const FGameplayAbilityTargetDataHandle LocalTargetDataHandle(MoveTemp(const_cast<FGameplayAbilityTargetDataHandle&>(InData)));
+
+	// if this isn't the local player on the server, then notify the server
+	if (bShouldNotifyServer)
+		ASC->CallServerSetReplicatedTargetData(CurrentSpecHandle, CurrentActivationInfo.GetActivationPredictionKey(), LocalTargetDataHandle, ApplicationTag, ASC->ScopedPredictionKey);
+
+	// Execute the ability we've now successfully committed
+	ActivateAbilityWithTargetData(LocalTargetDataHandle, ApplicationTag);
+
+	// We've processed the data, clear it from the RPC buffer
+	ASC->ConsumeClientReplicatedTargetData(CurrentSpecHandle, CurrentActivationInfo.GetActivationPredictionKey());
+}
+
+void UFPGAGameplayAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	// UE_LOG(LogTemp, Warning, TEXT("Activate ability"));
+	// if (bHasBlueprintActivate)
+	// {
+	// 	K2_ActivateAbility();
+	// 	return;
+	// }
+	//
+	// if (bHasBlueprintActivateFromEvent && TriggerEventData)
+	// {
+	// 	K2_ActivateAbilityFromEvent(*TriggerEventData);
+	// 	return;
+	// }
+
+	if (ActorInfo->IsLocallyControlledPlayer())
+	{
+		ActivateLocalPlayerAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	}
+	else if (ActorInfo->IsNetAuthority())
+	{
+		ActivateServerAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	}
+	else
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, false, true);
+	}
+}
+
+void UFPGAGameplayAbility::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	UAbilitySystemComponent* ASC = CurrentActorInfo->AbilitySystemComponent.Get();
+	check(ASC);
+
+	// clean up the mess we made in ASC
+	ASC->AbilityTargetDataSetDelegate(CurrentSpecHandle, CurrentActivationInfo.GetActivationPredictionKey()).Remove(NotifyTargetDataReadyDelegateHandle);
+	ASC->ConsumeClientReplicatedTargetData(CurrentSpecHandle, CurrentActivationInfo.GetActivationPredictionKey());
+
+	// run base class cleanup operations too
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
---
+++ Source/FPGameplayAbilities/Public/AbilitySystem/FPGAGameplayAbility.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "FPGAGameplayAbility.generated.h"
+
+/**
+ * See https://x157.github.io/UE5/GameplayAbilitySystem/How-To-Send-Client-Gameplay-Ability-Data-to-Server-in-C++
+ */
+UCLASS()
+class FPGAMEPLAYABILITIES_API UFPGAGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	bool bHasBlueprintActivateAbilityWithTargetData;
+	UFPGAGameplayAbility();
+
+	virtual void ActivateLocalPlayerAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData);
+	virtual void ActivateServerAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData);
+
+	virtual void ActivateAbilityWithTargetData(const FGameplayAbilityTargetDataHandle& TargetDataHandle, FGameplayTag ApplicationTag);
+
+	UFUNCTION(BlueprintImplementableEvent, Category=Ability, DisplayName="Activate Ability From TargetData", meta=(ScriptName ="ActivateAbilityFromTargetData"))
+	void K2_ActivateAbilityFromTargetData(const FGameplayAbilityTargetDataHandle& TargetDataHandle, FGameplayTag ApplicationTag);
+
+	virtual void NotifyTargetDataReady(const FGameplayAbilityTargetDataHandle& InData, FGameplayTag ApplicationTag);
+
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	// ActivateServerAbility();
+	// EndAbilityClean
+
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
+private:
+	FDelegateHandle NotifyTargetDataReadyDelegateHandle;
+};
