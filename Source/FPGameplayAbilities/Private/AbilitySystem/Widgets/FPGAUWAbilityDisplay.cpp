58a2123962adc31c2efb152d89876fa70e7e4105
fpwong <func.pwong@gmail.com>
2022-07-18 11:46:02 +1000

    Implement ability display

---
+++ Source/FPGameplayAbilities/Private/AbilitySystem/Widgets/FPGAUWAbilityDisplay.cpp
@@ -0,0 +1,85 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AbilitySystem/Widgets/FPGAUWAbilityDisplay.h"
+
+#include "AbilitySystemComponent.h"
+#include "CommonTextBlock.h"
+#include "Abilities/GameplayAbility.h"
+#include "Components/ProgressBar.h"
+
+// TODO this doesn't work for non-instanced abilities?
+
+void UFPGAUWAbilityDisplay::OnAbilityEnded(UGameplayAbility* GameplayAbility)
+{
+		// UE_LOG(LogTemp, Warning, TEXT("Ability ended too"));
+	GameplayAbility->OnGameplayAbilityEnded.RemoveAll(this);
+
+	if (ActiveAbility == GameplayAbility)
+	{
+		ActiveAbility = nullptr;
+	}
+}
+
+void UFPGAUWAbilityDisplay::OnAbilityActivate(UGameplayAbility* GameplayAbility)
+{
+	if (GameplayAbility->GetClass() == Ability->GetClass())
+	{
+		ActiveAbility = GameplayAbility;
+		// UE_LOG(LogTemp, Warning, TEXT("This is correct?"));
+		GameplayAbility->OnGameplayAbilityEnded.AddUObject(this, &ThisClass::OnAbilityEnded);
+	}
+}
+
+void UFPGAUWAbilityDisplay::InitAbility(UAbilitySystemComponent* InAbilitySystem, UGameplayAbility* InAbility)
+{
+	Ability = InAbility;
+	AbilitySystem = InAbilitySystem;
+
+	AbilitySystem->AbilityActivatedCallbacks.AddUObject(this, &ThisClass::OnAbilityActivate);
+
+	// auto ASC = Ability->GetAbilitySystemComponentFromActorInfo();
+	// Ability->OnGameplayAbilityEnded;
+
+	if (Ability && AbilityNameLabel)
+	{
+		AbilityNameLabel->SetText(FText::FromString(Ability->GetName()));
+	}
+
+	UpdateAbilityProgress();
+}
+
+void UFPGAUWAbilityDisplay::UpdateAbilityProgress()
+{
+	if (!Ability)
+	{
+		AbilityProgressBar->SetPercent(0);
+		return;
+	}
+
+	float TimeRemaining = 0;
+	float CooldownDuration = 0;
+
+	if (Ability->GetCooldownGameplayEffect())
+	{
+		Ability->GetCooldownTimeRemainingAndDuration(Ability->GetCurrentAbilitySpecHandle(), Ability->GetCurrentActorInfo(), TimeRemaining, CooldownDuration);
+	}
+
+	if (AbilityProgressBar)
+	{
+		const float Progress = TimeRemaining > 0 ? TimeRemaining / CooldownDuration : 0;
+		AbilityProgressBar->SetPercent(Progress);
+	}
+
+	if (CooldownLabel)
+	{
+		CooldownLabel->SetText(FText::FromString(FString::Printf(TEXT("%.1f"), TimeRemaining)));
+		CooldownLabel->SetVisibility(TimeRemaining > 0 ? ESlateVisibility::Visible : ESlateVisibility::Hidden);
+	}
+}
+
+void UFPGAUWAbilityDisplay::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+
+	UpdateAbilityProgress();
+}
---
+++ Source/FPGameplayAbilities/Public/AbilitySystem/Widgets/FPGAUWAbilityDisplay.h
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayAbilitySpec.h"
+#include "Blueprint/UserWidget.h"
+#include "FPGAUWAbilityDisplay.generated.h"
+
+class UGameplayAbility;
+class UProgressBar;
+class UCommonTextBlock;
+
+UCLASS(Abstract, meta = (Category = "FPGAButton"))
+class FPGAMEPLAYABILITIES_API UFPGAUWAbilityDisplay : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	UCommonTextBlock* CooldownLabel;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidgetOptional))
+	UProgressBar* AbilityProgressBar;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidgetOptional))
+	UCommonTextBlock* AbilityNameLabel;
+
+	void OnAbilityEnded(UGameplayAbility* GameplayAbility);
+	void OnAbilityActivate(UGameplayAbility* GameplayAbility);
+	void InitAbility(UAbilitySystemComponent* InAbilitySystem, UGameplayAbility* InAbility);
+
+	void UpdateAbilityProgress();
+
+protected:
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
+	FGameplayAbilitySpecHandle AbilitySpecHandle;
+	UGameplayAbility* Ability;
+	UGameplayAbility* ActiveAbility;
+	UAbilitySystemComponent* AbilitySystem;
+};
