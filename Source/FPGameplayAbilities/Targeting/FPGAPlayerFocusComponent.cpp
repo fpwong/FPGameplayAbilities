d1103c32e090f697225a06383f7618a364c1b0a1
fpwong <func.pwong@gmail.com>
2022-07-19 23:11:36 +1000

    Add player focus component

---
+++ Source/FPGameplayAbilities/Targeting/FPGAPlayerFocusComponent.cpp
@@ -0,0 +1,59 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPGAPlayerFocusComponent.h"
+
+// Sets default values for this component's properties
+UFPGAPlayerFocusComponent::UFPGAPlayerFocusComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+// Called when the game starts
+void UFPGAPlayerFocusComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UFPGAPlayerFocusComponent::UpdateStateFromHitResults(const TArray<FHitResult>& HitResults)
+{
+	for (const auto& HitResult : HitResults)
+	{
+		if (auto HitActor = HitResult.GetActor())
+		{
+			if (TargetFilter.DoesFilterPass(nullptr, HitActor))
+			{
+				if (HoveredActor.Get() == HitActor)
+				{
+					return;
+				}
+
+				if (HoveredActor.IsValid())
+				{
+					if (HoveredActor.Get()->Implements<UFPGAPlayerFocusInterface>())
+					{
+						IFPGAPlayerFocusInterface::Execute_OnEndFocus(HoveredActor.Get());
+					}
+
+					HoveredActor.Reset();
+				}
+
+				OnHoveredActorChanged.Broadcast(HoveredActor.Get(), HitActor);
+
+				HoveredActor = HitActor;
+				if (HitActor->Implements<UFPGAPlayerFocusInterface>())
+				{
+					IFPGAPlayerFocusInterface::Execute_OnBeginFocus(HitActor);
+				}
+
+				return;
+			}
+		}
+	}
+
+	if (HoveredActor.IsValid())
+	{
+		OnHoveredActorChanged.Broadcast(HoveredActor.Get(), nullptr);
+		HoveredActor = nullptr;
+	}
+}
+
---
+++ Source/FPGameplayAbilities/Targeting/FPGAPlayerFocusComponent.h
@@ -0,0 +1,62 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FPGATargetFilter.h"
+#include "Components/ActorComponent.h"
+#include "FPGAPlayerFocusComponent.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class FPGAMEPLAYABILITIES_API UFPGAPlayerFocusInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class FPGAMEPLAYABILITIES_API IFPGAPlayerFocusInterface
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category="Player Focus")
+	void OnBeginHovered();
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category="Player Focus")
+	void OnEndHovered();
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category="Player Focus")
+	void OnBeginFocus();
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category="Player Focus")
+	void OnEndFocus();
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FFPOnHoveredActorChanged, AActor*, OldActor, AActor*, NewActor);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class FPGAMEPLAYABILITIES_API UFPGAPlayerFocusComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FFPOnHoveredActorChanged OnHoveredActorChanged;
+
+	UFPGAPlayerFocusComponent();
+
+	virtual void UpdateStateFromHitResults(const TArray<FHitResult>& HitResults);
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	/** Actor currently focused by the player. */
+	TWeakObjectPtr<AActor> FocusedTarget;
+
+	/** Actor currently hovered by the player. */
+	TWeakObjectPtr<AActor> HoveredActor;
+
+	UPROPERTY(EditDefaultsOnly)
+	FFPGATargetFilter TargetFilter;
+};
