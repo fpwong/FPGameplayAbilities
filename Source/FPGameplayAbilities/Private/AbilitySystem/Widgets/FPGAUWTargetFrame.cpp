97ad2f1575856c0eafaba955c1ee04e3657c3a3e
fpwong <func.pwong@gmail.com>
2022-07-20 17:51:34 +1000

    Add target frame

---
+++ Source/FPGameplayAbilities/Private/AbilitySystem/Widgets/FPGAUWTargetFrame.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AbilitySystem/Widgets/FPGAUWTargetFrame.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+
+void UFPGAUWTargetFrame::NativeConstruct()
+{
+	Super::NativeConstruct();
+	SetVisibility(ESlateVisibility::Hidden);
+}
+
+void UFPGAUWTargetFrame::SetActor_Implementation(AActor* InTargetActor)
+{
+	if (InTargetActor != TargetActor)
+	{
+		TargetActor = InTargetActor;
+
+		SetVisibility(TargetActor ? ESlateVisibility::Visible : ESlateVisibility::Hidden);
+
+		UAbilitySystemComponent* AbilitySystem = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(TargetActor);
+		check(HealthBar);
+		HealthBar->BindAttributes(AbilitySystem);
+
+		check(StatusBar);
+		StatusBar->SetActor(InTargetActor);
+	}
+}
---
+++ Source/FPGameplayAbilities/Public/AbilitySystem/Widgets/FPGAUWTargetFrame.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FPGAAttributeBar.h"
+#include "FPGAUWStatusBar.h"
+#include "Blueprint/UserWidget.h"
+#include "FPGAUWTargetFrame.generated.h"
+
+UCLASS()
+class FPGAMEPLAYABILITIES_API UFPGAUWTargetFrame : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadOnly)
+	AActor* TargetActor;
+
+	UPROPERTY(BlueprintReadOnly)
+	UAbilitySystemComponent* TargetAbilitySystem = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	UFPGAAttributeBar* HealthBar;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	UFPGAUWStatusBar* StatusBar;
+
+	virtual void NativeConstruct() override;
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void SetActor(AActor* InTargetActor);
+};
