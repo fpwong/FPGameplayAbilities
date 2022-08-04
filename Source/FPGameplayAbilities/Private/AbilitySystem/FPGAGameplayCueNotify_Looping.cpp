e2ddd433d1b2dda1fb662e782a722df95b68f0c7
fpwong <func.pwong@gmail.com>
2022-08-04 22:11:35 +1000

    Add FPGAGameplayCueNotify with relative offset

---
+++ Source/FPGameplayAbilities/Private/AbilitySystem/FPGAGameplayCueNotify_Looping.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AbilitySystem/FPGAGameplayCueNotify_Looping.h"
+
+#include "GameFramework/Character.h"
+
+FGameplayCueParameters AFPGAGameplayCueNotify_Looping::MakeModifiedParameters(AActor* Target, const FGameplayCueParameters& Parameters)
+{
+	FGameplayCueParameters Modified = Parameters;
+	const float TargetHeight = Target->GetSimpleCollisionHalfHeight();
+	Modified.Location = FVector(0, 0, TargetHeight * RelativeHeightOffset);
+	return Modified;
+}
+
+bool AFPGAGameplayCueNotify_Looping::OnActive_Implementation(AActor* Target, const FGameplayCueParameters& Parameters)
+{
+	return Super::OnActive_Implementation(Target, bUseRelativeHeightOffset ? MakeModifiedParameters(Target, Parameters) : Parameters);
+}
+
+bool AFPGAGameplayCueNotify_Looping::WhileActive_Implementation(AActor* Target, const FGameplayCueParameters& Parameters)
+{
+	return Super::WhileActive_Implementation(Target, bUseRelativeHeightOffset ? MakeModifiedParameters(Target, Parameters) : Parameters);
+}
+
+bool AFPGAGameplayCueNotify_Looping::OnExecute_Implementation(AActor* Target, const FGameplayCueParameters& Parameters)
+{
+	return Super::OnExecute_Implementation(Target, bUseRelativeHeightOffset ? MakeModifiedParameters(Target, Parameters) : Parameters);
+}
---
+++ Source/FPGameplayAbilities/Public/AbilitySystem/FPGAGameplayCueNotify_Looping.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayCueNotify_Looping.h"
+#include "FPGAGameplayCueNotify_Looping.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPGAMEPLAYABILITIES_API AFPGAGameplayCueNotify_Looping : public AGameplayCueNotify_Looping
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Meta = (InlineEditConditionToggle))
+	uint32 bUseRelativeHeightOffset : 1;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Meta = (EditCondition = "bUseRelativeHeightOffset"))
+	float RelativeHeightOffset = 1.0f;
+
+	FGameplayCueParameters MakeModifiedParameters(AActor* Target, const FGameplayCueParameters& Parameters);
+
+	virtual bool OnActive_Implementation(AActor* Target, const FGameplayCueParameters& Parameters) override;
+	virtual bool WhileActive_Implementation(AActor* Target, const FGameplayCueParameters& Parameters) override;
+	virtual bool OnExecute_Implementation(AActor* Target, const FGameplayCueParameters& Parameters) override;
+};
