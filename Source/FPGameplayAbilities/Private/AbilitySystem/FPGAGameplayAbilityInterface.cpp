164788a83e8e0af2f3d3e27c768e1baeff011196
fpwong <func.pwong@gmail.com>
2022-08-06 12:25:09 +1000

    Add interface to get ability by tag

---
+++ Source/FPGameplayAbilities/Private/AbilitySystem/FPGAGameplayAbilityInterface.cpp
@@ -0,0 +1,5 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AbilitySystem/FPGAGameplayAbilityInterface.h"
+
+// Add default functionality here for any IFPGAGameplayAbilityInterface functions that are not pure virtual.
---
+++ Source/FPGameplayAbilities/Public/AbilitySystem/FPGAGameplayAbilityInterface.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "FPGAGameplayAbilityInterface.generated.h"
+
+struct FGameplayTag;
+class UGameplayAbility;
+
+UINTERFACE()
+class FPGAMEPLAYABILITIES_API UFPGAGameplayAbilityInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class FPGAMEPLAYABILITIES_API IFPGAGameplayAbilityInterface
+{
+	GENERATED_BODY()
+
+public:
+	virtual TSubclassOf<UGameplayAbility> GetAbilityByTag(FGameplayTag GameplayTag) = 0; 
+};
