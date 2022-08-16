4754ea6605b61db2764b232d9e755f08acf9a73e
fpwong <func.pwong@gmail.com>
2022-08-16 21:33:59 +1000

    Add unused gameplay cue manager implementation

---
+++ Source/FPGameplayAbilities/Private/AbilitySystem/FPGAGameplayCueManager.cpp
@@ -0,0 +1,8 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AbilitySystem/FPGAGameplayCueManager.h"
+
+void UFPGAGameplayCueManager::FlushPendingCues()
+{
+	Super::FlushPendingCues();
+}
---
+++ Source/FPGameplayAbilities/Public/AbilitySystem/FPGAGameplayCueManager.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayCueManager.h"
+#include "FPGAGameplayCueManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPGAMEPLAYABILITIES_API UFPGAGameplayCueManager : public UGameplayCueManager
+{
+	GENERATED_BODY()
+
+public:
+	virtual void FlushPendingCues() override;
+};
