2ddc4276999144383ec10f256667f392d621cb07
fpwong <func.pwong@gmail.com>
2023-02-17 21:09:48 +1100

    Add ability ready binding

---
+++ Source/FPGameplayAbilities/Private/AbilitySystem/FPGATypes.cpp
@@ -0,0 +1,94 @@
+﻿#include "FPGATypes.h"
+
+#include "AbilitySystemComponent.h"
+#include "Abilities/GameplayAbility.h"
+#include "AbilitySystem/FPGAGameplayAbilitiesLibrary.h"
+
+void FFPGAAbilityReadyBinding::Clear()
+{
+	OnAbilityReady.Clear();
+	BoundAbility.Reset();
+
+	// if (BoundAbilitySystem.IsValid())
+	// {
+	// 	for (FActiveGameplayEffectHandle& BoundHandle : BoundHandles)
+	// 	{
+	// 		BoundCooldownDelegates.Add(BoundAbilitySystem.Get()->OnGameplayEffectRemoved_InfoDelegate(BoundHandle)->RemoveAll(this));
+	// 	}
+	// }
+
+	BoundCooldownDelegates.Reset();
+}
+
+void FFPGAAbilityReadyBinding::BindToGameplayAbility(UGameplayAbility* Ability)
+{
+	if (!Ability)
+	{
+		return;
+	}
+
+	UAbilitySystemComponent* AbilitySystem = Ability->GetAbilitySystemComponentFromActorInfo();
+	if (!ensure(AbilitySystem))
+	{
+		return;
+	}
+
+	if (BoundAbility == Ability)
+	{
+		return;
+	}
+
+	BoundAbility = Ability;
+	
+	// UE_LOG(LogTemp, Warning, TEXT("bound to ability?"));
+
+	// bind to cooldown
+	if (const FGameplayTagContainer* CooldownTags = Ability->GetCooldownTags())
+	{
+		// UE_LOG(LogTemp, Warning, TEXT("Found cooldowns"));
+		FGameplayEffectQuery CooldownQuery;
+		CooldownQuery.OwningTagQuery = FGameplayTagQuery::MakeQuery_MatchAllTags(*CooldownTags);
+
+		TArray<FActiveGameplayEffectHandle> BlockingEffects = AbilitySystem->GetActiveEffects(CooldownQuery);
+
+		for (FActiveGameplayEffectHandle Handle : BlockingEffects)
+		{
+			// UE_LOG(LogTemp, Warning, TEXT("Bound to handle!"));
+			BoundHandles.Add(Handle);
+			BoundCooldownDelegates.Add(AbilitySystem->OnGameplayEffectRemoved_InfoDelegate(Handle)->AddRaw(this, &FFPGAAbilityReadyBinding::OnCooldownEffectRemoved));
+		}
+	}
+
+	Ability->OnGameplayAbilityEnded.AddRaw(this, &FFPGAAbilityReadyBinding::OnAbilityEnded);
+
+	// bind to cost
+}
+
+void FFPGAAbilityReadyBinding::OnCooldownEffectRemoved(const FGameplayEffectRemovalInfo& GameplayEffectRemovalInfo)
+{
+	// UE_LOG(LogTemp, Warning, TEXT("cooldown removed!"));
+	CheckAbilityReady();
+}
+
+void FFPGAAbilityReadyBinding::CheckAbilityReady()
+{
+	if (BoundAbility.IsValid())
+	{
+		if (UFPGAGameplayAbilitiesLibrary::CanActivateAbility(BoundAbility->GetAbilitySystemComponentFromActorInfo(), BoundAbility.Get()))
+		{
+			UGameplayAbility* Ability = BoundAbility.Get();
+			BoundAbility.Reset();
+			OnAbilityReady.Broadcast(Ability);
+		}
+	}
+}
+
+void FFPGAAbilityReadyBinding::OnAbilityEnded(UGameplayAbility* GameplayAbility)
+{
+	// UE_LOG(LogTemp, Warning, TEXT("Ability ended!"));
+	if (BoundAbility.IsValid())
+	{
+		GameplayAbility->OnGameplayAbilityEnded.RemoveAll(this);
+		CheckAbilityReady();
+	}
+}
---
+++ Source/FPGameplayAbilities/Private/AbilitySystem/FPGATypes.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+#include "GameplayEffectTypes.h"
+
+#include "FPGATypes.generated.h"
+
+class UGameplayAbility;
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FFPOnAbilityReady, UGameplayAbility*);
+
+UENUM(BlueprintType)
+enum class EFPGACancelLevel : uint8
+{
+	None,
+	OnlyBackswing,
+	All
+};
+USTRUCT(BlueprintType)
+struct FPGAMEPLAYABILITIES_API FFPGAAbilityReadyBinding
+{
+	GENERATED_BODY()
+
+	FFPOnAbilityReady OnAbilityReady;
+
+	TWeakObjectPtr<UGameplayAbility> BoundAbility;
+	TWeakObjectPtr<UAbilitySystemComponent> BoundAbilitySystem;
+
+	bool IsBound() { return BoundAbility.IsValid(); }
+
+	void Clear();
+
+	void BindToGameplayAbility(UGameplayAbility* Ability);
+
+	void OnCooldownEffectRemoved(const FGameplayEffectRemovalInfo& GameplayEffectRemovalInfo);
+
+	void CheckAbilityReady();
+
+protected:
+	void OnAbilityEnded(UGameplayAbility* GameplayAbility);
+
+	TSet<FDelegateHandle> BoundCooldownDelegates;
+	TSet<FActiveGameplayEffectHandle> BoundHandles;
+};
