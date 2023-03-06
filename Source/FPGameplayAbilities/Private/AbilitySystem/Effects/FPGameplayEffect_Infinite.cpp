a6840e981641cc4f359b60193c02f3257445ab99
fpwong <func.pwong@gmail.com>
2023-03-07 01:03:03 +1100

    Add infinite gameplay effect template

---
+++ Source/FPGameplayAbilities/Private/AbilitySystem/Effects/FPGameplayEffect_Infinite.cpp
@@ -0,0 +1,10 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AbilitySystem/Effects/FPGameplayEffect_Infinite.h"
+
+#include "GameplayEffect.h"
+
+UFPGameplayEffect_Infinite::UFPGameplayEffect_Infinite()
+{
+	DurationPolicy = EGameplayEffectDurationType::Infinite; 
+}
---
+++ Source/FPGameplayAbilities/Public/AbilitySystem/Effects/FPGameplayEffect_Infinite.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "GameplayEffect.h"
+#include "FPGameplayEffect_Infinite.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPGAMEPLAYABILITIES_API UFPGameplayEffect_Infinite : public UGameplayEffect
+{
+	GENERATED_BODY()
+
+	UFPGameplayEffect_Infinite();
+};
