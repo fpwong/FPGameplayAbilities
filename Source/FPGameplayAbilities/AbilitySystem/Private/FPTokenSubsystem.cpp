13d49853202e3560aa66325757a7f036dbb3f164
fpwong <func.pwong@gmail.com>
2023-10-16 12:58:24 +1100

    Add token subsystem that dynamically generates a gameplay effect from a tag and attribute

---
+++ Source/FPGameplayAbilities/AbilitySystem/Private/FPTokenSubsystem.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPTokenSubsystem.h"
+
+#include "GameplayEffect.h"
+
+UGameplayEffect* UFPTokenSubsystem::FindOrAddTokenEffect(FGameplayTag Tag, FGameplayAttribute Attribute)
+{
+	if (TokenMap.Contains(Tag))
+	{
+		return TokenMap[Tag];
+	}
+
+	FString Name = Tag.GetTagName().ToString() + "_Token";
+
+	UGameplayEffect* TokenEffect = NewObject<UGameplayEffect>(GetTransientPackage(), FName(Name));
+	TokenEffect->DurationPolicy = EGameplayEffectDurationType::Infinite;
+	TokenEffect->StackingType = EGameplayEffectStackingType::AggregateByTarget;
+
+	FGameplayModifierInfo ModInfo;
+	ModInfo.Attribute = Attribute;
+	ModInfo.ModifierMagnitude = FGameplayEffectModifierMagnitude(FScalableFloat(1.0)); 
+	ModInfo.SourceTags.RequireTags.AddTag(Tag);
+	TokenEffect->Modifiers.Add(ModInfo);
+
+	TokenMap.Add(Tag, TokenEffect);
+	return TokenEffect;
+}
---
+++ Source/FPGameplayAbilities/AbilitySystem/Private/FPTokenSubsystem.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Subsystems/EngineSubsystem.h"
+#include "FPTokenSubsystem.generated.h"
+
+struct FGameplayAttribute;
+class UGameplayEffect;
+
+// TODO test if this works for a networked game
+UCLASS()
+class FPGAMEPLAYABILITIES_API UFPTokenSubsystem : public UEngineSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	UGameplayEffect* FindOrAddTokenEffect(FGameplayTag Tag, FGameplayAttribute Attribute);
+
+	UPROPERTY()
+	TMap<FGameplayTag, UGameplayEffect*> TokenMap;
+};
