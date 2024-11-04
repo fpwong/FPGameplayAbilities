9e745e6f6eca073688475a022b0046fe6f2f8184
fpwong <func.pwong@gmail.com>
2024-11-04 11:35:29 +1100

    Add simple interaction component

---
+++ Source/FPGameplayAbilities/Core/FPInteractionComponent.cpp
@@ -0,0 +1,100 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPInteractionComponent.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
+bool UFPInteractionComponent::TryInteractWithFocus()
+{
+	if (FocusedActor.IsValid())
+	{
+		if (FocusedActor.IsValid() && FocusedActor->Implements<UFPInteractInterface>())
+		{
+			IFPInteractInterface::Execute_OnInteract(FocusedActor.Get(), GetOwner());
+		}
+
+		return true;
+	}
+
+	return false;
+}
+
+UFPInteractionComponent::UFPInteractionComponent(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UFPInteractionComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	OnBeginOverlap.AddDynamic(this, &ThisClass::BeginOverlap);
+	OnEndOverlap.AddDynamic(this, &ThisClass::EndOverlap);
+}
+
+void UFPInteractionComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	AActor* NewFocus = FocusedActor.Get();
+	float Closest = GetDistToActor(FocusedActor.Get());
+
+	for (AActor* Actor : GetOverlappingActors())
+	{
+		const float Dist = GetDistToActor(Actor);
+		if (Dist < Closest)
+		{
+			Closest = Dist;
+			NewFocus = Actor;
+		}
+	}
+
+	ChangeFocusedActor(FocusedActor.Get(), NewFocus);
+}
+
+void UFPInteractionComponent::BeginOverlap(AActor* Actor)
+{
+	if (Actor && !FocusedActor.IsValid())
+	{
+		ChangeFocusedActor(FocusedActor.Get(), Actor);
+	}
+}
+
+void UFPInteractionComponent::EndOverlap(AActor* Actor)
+{
+	if (Actor && (Actor == FocusedActor))
+	{
+		ChangeFocusedActor(FocusedActor.Get(), nullptr);
+	}
+}
+
+void UFPInteractionComponent::ChangeFocusedActor(AActor* Old, AActor* New)
+{
+	if (FocusedActor != New)
+	{
+		FocusedActor = New;
+		OnFocusedActorChanged.Broadcast(Old, New);
+
+		if (Old && Old->Implements<UFPInteractInterface>())
+		{
+			IFPInteractInterface::Execute_OnLoseFocus(Old);
+		}
+
+		if (New && New->Implements<UFPInteractInterface>())
+		{
+			IFPInteractInterface::Execute_OnGainFocus(New);
+		}
+	}
+}
+
+float UFPInteractionComponent::GetDistToActor(AActor* Actor)
+{
+	if (!Actor)
+	{
+		return FLT_MAX;
+	}
+
+	return FVector::Dist(GetOwner()->GetActorLocation(), Actor->GetActorLocation());
+}
---
+++ Source/FPGameplayAbilities/Core/FPInteractionComponent.h
@@ -0,0 +1,66 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FPGAOverlapManager.h"
+#include "UObject/Object.h"
+#include "FPInteractionComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FFPOnFocusedActorChanged, AActor*, Old, AActor*, New);
+
+UINTERFACE()
+class UFPInteractInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class IFPInteractInterface
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintNativeEvent)
+	void OnGainFocus();
+
+	UFUNCTION(BlueprintNativeEvent)
+	void OnLoseFocus();
+
+	UFUNCTION(BlueprintNativeEvent)
+	void OnInteract(AActor* Source);
+};
+
+UCLASS(BlueprintType, meta = (BlueprintSpawnableComponent))
+class FPGAMEPLAYABILITIES_API UFPInteractionComponent : public UFPGAOverlapManager
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere)
+	TWeakObjectPtr<AActor> FocusedActor;
+
+	UPROPERTY(BlueprintAssignable)
+	FFPOnFocusedActorChanged OnFocusedActorChanged;
+
+	UFUNCTION(BlueprintCallable)
+	bool TryInteractWithFocus();
+
+	UFUNCTION(BlueprintCallable)
+	AActor* GetFocusedActor() const { return FocusedActor.Get(); };
+
+	UFPInteractionComponent(const FObjectInitializer& ObjectInitializer);
+
+	virtual void BeginPlay() override;
+
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION()
+	void BeginOverlap(AActor* Actor);
+
+	UFUNCTION()
+	void EndOverlap(AActor* Actor);
+
+	void ChangeFocusedActor(AActor* Old, AActor* New);
+
+	float GetDistToActor(AActor* Actor);
+};
